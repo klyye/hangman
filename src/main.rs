e5d5e41ad9da3178ad0136fdd728129b1242d3d6
Kyle Zhang <kylezzhang@berkeley.edu>
2023-03-11 15:53:02 -0800

    actually kinda works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 // https://stackoverflow.com/questions/27318076/edit-string-in-place-with-a-function
 fn all_underscores(s: &str) -> String {
@@ -9,28 +10,56 @@ fn all_underscores(s: &str) -> String {
     r
 }
 
-fn main() {
-    let word_to_guess = "crustacean";
-    let guessed = all_underscores(word_to_guess);
+fn guess_char(guessed: &str, correct_word: &str, guess: char) -> String {
+    let mut r = String::with_capacity(guessed.len());
+    for (i, c) in correct_word.char_indices() {
+        r.push(if c == guess { c } else { guessed.chars().nth(i).unwrap() });
+    }
+    r
+}
 
-    loop {
-        println!("{}", guessed);
-        println!("Please input your guess: ");
+fn char_input() -> char {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
-        let mut input = String::new();
-
+    while input.trim().len() != 1 {
+        println!("Please only enter one character.");
+        input.clear();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
-        let trimmed_input = input.trim();
+    }
+    input.trim().to_lowercase().chars().nth(0).unwrap()
+}
 
-        if trimmed_input.len() != 1 {
-            println!("Please only enter one character.")
-        } else if trimmed_input == word_to_guess {
-            println!("Hooray, you guessed the correct word!");
-            break;
+fn main() {
+    let word_to_guess = "crustacean";
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    let mut guessed = all_underscores(word_to_guess);
+    let mut remaining_guesses = word_to_guess.len() + 3;
+
+    loop {
+        println!("\n{}", guessed);
+        println!("{remaining_guesses} guesses remaining.\n");
+        println!("Please input your guess: ");
+
+        let guess = char_input();
+        if word_to_guess.contains(guess) {
+            println!("Nice! The word contains {guess}!");
+            guessed = guess_char(&guessed, word_to_guess, guess);
         } else {
-            println!("Sorry, {trimmed_input} is not the correct word!");
+            println!("Sorry, the word does not contain {guess}!");
+        }
+
+        remaining_guesses -= 1;
+        if !guessed.contains('_') {
+            println!("Congrats, you won!");
+            break;
+        } else if remaining_guesses <= 0 {
+            println!("Sorry, you ran out of guesses! The word was {word_to_guess}.");
+            break;
         }
     }
 }
